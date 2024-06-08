3f6f78a1c27caee8feb84fe4a741dccb1e326756
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-06-07 19:31:14 -0700

    visual testing 1

--- cypress/integration/visualTest.spec.js
+++ cypress/integration/visualTest.spec.js
@@ -2,19 +2,17 @@
 describe('visual test', () => {
 
     it.only('should test snapshot', () => {
-
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
 
         cy.contains('nb-card', 'Using the Grid').then(firstForm => {
-            cy.wrap(firstForm).toMatchImageSnapshot()
-            cy.document().toMatchImageSnapshot()
+            cy.wrap(firstForm).toMatchImageSnapshot()   // snapshot only testing area
+            cy.document().toMatchImageSnapshot()        // for full page
         })
     })
 
     it('should test with Percy', () => {
-
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
