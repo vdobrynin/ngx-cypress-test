78ed9aeaba1e06405373f7be122ae40306a7291b
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-02-28 21:12:11 -0800

    review json objects v1

--- cypress/integration/jsonObject.spec.js
+++ cypress/integration/jsonObject.spec.js
@@ -1,18 +1,12 @@
 /// <reference types="cypress" />
 
 describe('JSON objects', () => {
-
   it('JSON objects', () => {
-
     cy.openHomePage()
     const simpleObject = { "key": "value", "key2": "value2" }
-
     const simpleArrayOfValues = ["one", "two", "three"]
-
     const arrayOfObjects = [{ "key": "value" }, { "key2": "value2" }, { "key3": "value3" }]
-
     const typesOfData = { "string": "this is a string", "number": 10 }
-
     const mix = {
       "FirstName": "Joe",
       "LastName": "Doe",
@@ -34,7 +28,6 @@ describe('JSON objects', () => {
     console.log(simpleArrayOfValues[2])
     console.log(arrayOfObjects[2].key3)
     console.log(mix.Students[1].lastName)
-
     const lastNameOfSecondStudent = mix.Students[1].firstName
   })
 })
