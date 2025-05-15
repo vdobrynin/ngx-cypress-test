4f22c4cf478c6cb6bbea4de4553be4c5c9b6969c
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-15 16:16:29 -0700

    review #51 shadow dom

--- cypress/integration/shadow.spec.js
+++ cypress/integration/shadow.spec.js
@@ -2,16 +2,21 @@
 describe("shadow dom", () => {
 
   it.only("access shadow dom", () => {
-    cy.visit("https://www.htmlelements.com/demos/dropdownlist/shadow-dom/index.htm")
+    // cy.visit("https://www.htmlelements.com/demos/dropdownlist/shadow-dom/index.htm")
 
-    cy.wait(3000);
-    cy.get('[class="outlined smart-ui-component"]')
-      .shadow()
-      .find('[smart-id="actionButton"]')
-      .click()
-      .should('contain', 'Mobile')
-    cy.percySnapshot('FormLayouts1');
-    cy.percySnapshot('FormLayouts2', { widths: [768, 992, 1200] });
+    // cy.wait(3000);
+    // cy.get("#Your project: 'Test Project'")
+    //   .shadow()
+    //   .find('#dropDownList62c6')
+
+    // cy.wait(3000);
+    // cy.get('[class="outlined smart-ui-component"]')
+    //   .shadow()
+    //   .find('[smart-id="actionButton"]')
+    //   .click()
+    //   .should('contain', 'Mobile')
+    // cy.percySnapshot('FormLayouts1');
+    // cy.percySnapshot('FormLayouts2', { widths: [768, 992, 1200] });
 
     // cy.contains('contain', 'Mobile').then(shadowRoot => {
     //   cy.percySnapshot('FormLayouts');
@@ -25,20 +30,8 @@ describe("shadow dom", () => {
     // .find('[data-testid="ArrowDropDownIcon"]')
     // // cy.get('#shadow-root').shadow().find('#root')
 
-    // cy.visit("https://www.htmlelements.com/demos/menu/shadow-dom/index.htm")
-    // // //cy.visit('https://radogado.github.io/shadow-dom-demo/')
-
-    // cy.get('[class="smart-ui-component"]')
-    //   .shadow()
-    //   .find('[smart-id="container"]')
-    //   .find('[smart-id="mainContainer"]')
-    //   .find('[class="smart-element smart-menu-items-group smart-unselectable"]')
-    // //   .find('.smart-menu-items-group[level="1"]')
-    //   .find('[class="smart-menu-item-label-container"]')
-    //   .find('[class="smart-menu-item-label-element"]')
-    //   .contains('span', 'File') 
-    //   .click({ force: true})
-    //   .should('contain', 'File')
-    //   // .find('.smart-menu-drop-down[level="2"]')
+    cy.visit('https://radogado.github.io/shadow-dom-demo/')
+    
+    cy.get('#app').shadow().find('#container')
   });
 });
