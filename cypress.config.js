b48220b86aa57b801ec0cb6d72ad9ed01de91bf7
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-20 23:08:05 -0700

    1st time successfully parallel testing with chrome & firefox

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,15 @@
 const { defineConfig } = require('cypress')
-const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+const { initPlugin } = require('cypress-plugin-snapshots/plugin')
 
 module.exports = defineConfig({
   viewportHeight: 900, //1200,
   viewportWidth: 1440, //1920, 
-  screenshotOnRunFailure: true,
+  screenshotOnRunFailure: false,
   video: true,
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 10000,
   waitForAnimations: true,
-  experimentalWebKitSupport: true,
+  experimentalWebKitSupport: false,
   env: {
     'cypress-plugin-snapshots': {
       imageConfig: {
@@ -25,11 +25,11 @@ module.exports = defineConfig({
       '**/__snapshots__/*',
       '**/__image_snapshots__/*'
     ],
-    specPattern: "cypress/integration/**/*.spec.{js,jsx,ts,tsx}",
+    specPattern: 'cypress/integration/**/*.spec.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      initPlugin(on, config);
+      // initPlugin(on, config);
       // on("task", percyHealthCheck);
-      // return require('./cypress/plugins/index.js')(on, config);
+      return require('./cypress/plugins/index.js')(on, config);
     },
   },
   includeShadowDom: true,
