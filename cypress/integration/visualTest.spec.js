24dc26803c80503b8e8de8cae297bd2e060b163f
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-23 16:50:06 -0700

    rename visualTest to Old & fix sripts

--- cypress/integration/visualTest.spec.js
+++ cypress/integration/visualTest.spec.js
@@ -1,25 +1,9 @@
 describe('visual test', () => {
 
-    it('should test snapshot', () => {
+    it('test applitools 5', () => {
+        
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
-
-        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
-            cy.wrap(firstForm).toMatchImageSnapshot()   // snapshot only testing area
-            cy.document().toMatchImageSnapshot()        // for full page
-        })
-    })
-
-    it.only('should test with Percy', () => {
-        cy.visit('/')
-        cy.contains('Forms').click()
-        cy.contains('Form Layouts').click()
-
-        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
-            cy.wait(1000);
-            cy.percySnapshot('FormLayouts1');
-            cy.percySnapshot('FormLayouts2', { widths: [768, 992, 1200] });
-        })
     })
 })
